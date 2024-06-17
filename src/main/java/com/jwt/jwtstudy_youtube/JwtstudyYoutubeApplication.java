db0fc6a755cc31732e47ae8292a0fb646b51db4c
JinSeoHyun <15roa1121@gmail.com>
2024-06-17 14:40:08 +0900

    프로젝트 시작

---
+++ src/main/java/com/jwt/jwtstudy_youtube/JwtstudyYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.jwtstudy_youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtstudyYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtstudyYoutubeApplication.class, args);
+	}
+
+}
