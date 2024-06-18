2f9e2236ff953876ece6d0d06761be1978f8b337
JinSeoHyun <15roa1121@gmail.com>
2024-06-18 15:17:17 +0900

    jwt 로그인 구현 - UserRepository에서 기존 회원있는지 검색하는 메서드 작성

--- src/main/java/com/jwt/jwtstudy_youtube/repository/UserRepository.java
+++ src/main/java/com/jwt/jwtstudy_youtube/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.jwt.jwtstudy_youtube.repository;public class UserRepository {
+package com.jwt.jwtstudy_youtube.repository;
+
+import com.jwt.jwtstudy_youtube.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+    UserEntity findByUsername(String username);
 }
