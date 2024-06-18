491b6c7bfbd17a8b9c925763caf0dde70b6b08e4
JinSeoHyun <15roa1121@gmail.com>
2024-06-18 16:15:53 +0900

    회원 가입 로직 구현 - JoinController 구현

--- src/main/java/com/jwt/jwtstudy_youtube/controller/JoinController.java
+++ src/main/java/com/jwt/jwtstudy_youtube/controller/JoinController.java
@@ -1,2 +1,24 @@
-package com.jwt.jwtstudy_youtube.controller;public class JoinController {
+package com.jwt.jwtstudy_youtube.controller;
+
+import com.jwt.jwtstudy_youtube.dto.JoinDto;
+import com.jwt.jwtstudy_youtube.service.JoinService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody //API응답을 위해서
+public class JoinController {
+
+    private final JoinService joinService;
+
+    public JoinController(JoinService joinService) {
+        this.joinService = joinService;
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDto joinDto) {
+        joinService.joinProcess(joinDto);
+        return "ok";
+    }
 }
