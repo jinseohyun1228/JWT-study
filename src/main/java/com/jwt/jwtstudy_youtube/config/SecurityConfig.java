9346908648d9c25f36f3b2879f770e2bc48f3017
JinSeoHyun <15roa1121@gmail.com>
2024-06-18 16:51:10 +0900

    jwt 로그인 로직 구현 - LoginFilter 등록: 여기서 '/login'에 대한 처리

--- src/main/java/com/jwt/jwtstudy_youtube/config/SecurityConfig.java
+++ src/main/java/com/jwt/jwtstudy_youtube/config/SecurityConfig.java
@@ -1,22 +1,37 @@
 package com.jwt.jwtstudy_youtube.config;
 
+import com.jwt.jwtstudy_youtube.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity //security를 위한 것이라고 알려주는 기능
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
 
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
@@ -29,13 +44,24 @@ public class SecurityConfig {
                 .formLogin((auth) -> auth.disable())
                 .httpBasic((auth) -> auth.disable())
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "join").permitAll()
+                        .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 //jwt는 세션리스 서버!!!
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+
+        //jwt로그인을 위한 필터등록
+        http.addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+        /*
+        * LogoutFilter는 인자가 필요~!~!
+        * 따라서 AuthenticationManager을 빈으로 등록해야한다.
+        */
+
+        /*
+        * authenticationManager도 인자가 필요하다.
+        */
         return http.build();
     }
 }
