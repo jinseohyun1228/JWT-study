e3297f32524779c403e25f788dabd356d51b47b8
JinSeoHyun <15roa1121@gmail.com>
2024-06-18 16:17:47 +0900

    회원 가입 로직 구현 - JoinDto 구현

---
+++ src/main/java/com/jwt/jwtstudy_youtube/dto/JoinDto.java
@@ -0,0 +1,11 @@
+package com.jwt.jwtstudy_youtube.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JoinDto {
+    private String username;
+    private String password;
+}
