c81d494b0636d6c0a55325e3c312b393ff3b1bcb
JinSeoHyun <15roa1121@gmail.com>
2024-06-19 19:08:58 +0900

    CORS 설정 - SecurityConfig : 시큐리티에서 설정, 시큐리티 필터를 사용하는 경우

--- src/main/java/com/jwt/jwtstudy_youtube/config/SecurityConfig.java
+++ src/main/java/com/jwt/jwtstudy_youtube/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.jwt.jwtstudy_youtube.config;
 import com.jwt.jwtstudy_youtube.jwt.JwtFilter;
 import com.jwt.jwtstudy_youtube.jwt.JwtUtil;
 import com.jwt.jwtstudy_youtube.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,6 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity //security를 위한 것이라고 알려주는 기능
@@ -62,6 +67,28 @@ public class SecurityConfig {
 
         http
                 .addFilterBefore(new JwtFilter(jwtUtil), LoginFilter.class);
+
+        // CORS 설정
+
+        http
+                .cors((corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); //허용할 프론트 서버 번호
+                        configuration.setAllowedMethods(Collections.singletonList("*")); //모든메서드 허용
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L); //시간
+
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization")); //헤더 허용... 졸료
+
+                        return configuration;
+                    }
+                })));
 
         return http.build();
     }
