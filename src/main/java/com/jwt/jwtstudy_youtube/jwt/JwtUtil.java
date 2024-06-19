b2d1e705169dc1aadabd8b768b38701269423628
JinSeoHyun <15roa1121@gmail.com>
2024-06-19 16:52:32 +0900

    jwt 발급과 검증 구현 - jwt를 검증하고 발급하는 JwtUtil 구현

---
+++ src/main/java/com/jwt/jwtstudy_youtube/jwt/JwtUtil.java
@@ -0,0 +1,60 @@
+package com.jwt.jwtstudy_youtube.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private SecretKey secretKey;
+
+    //생성자
+    public JwtUtil(@Value("${spring.jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    //코튼 만들기
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis())) // 토큰 발생시간
+                .setExpiration(new Date(System.currentTimeMillis() + expiredMs))// 소명 시간까지
+                .signWith(secretKey) // 시그니처~!
+                .compact();
+    }
+    //검증 진행
+    public String getUsername(String token) {
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("username", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().get("role", String.class);
+    }
+
+    //아직 유효한지
+
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+}
