992ee3c4ef27b67e0dbbcf424ada060818c0c6b1
JinSeoHyun <15roa1121@gmail.com>
2024-06-19 18:23:48 +0900

    jwt 필터 통과후 일시적인 세견에 대한 테스트 구현

--- src/main/java/com/jwt/jwtstudy_youtube/controller/AdminController.java
+++ src/main/java/com/jwt/jwtstudy_youtube/controller/AdminController.java
@@ -1,8 +1,14 @@
 package com.jwt.jwtstudy_youtube.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 @Controller
 @ResponseBody
@@ -10,7 +16,16 @@ public class AdminController {
 
     @GetMapping("/admin")
     public String admin() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
-        return "admin Controller";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+
+        String role = auth.getAuthority();
+
+        return "현재 사용자의 이름: " + username + " 현재 사용자의 권한: "+role;
     }
 }
