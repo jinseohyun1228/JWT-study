7b13031d1fde3519ec9bf6e124b360949d22ea21
JinSeoHyun <15roa1121@gmail.com>
2024-06-19 16:57:49 +0900

    jwt 발급과 검증 구현 - 로그인 성공시 jwt 발급

--- src/main/java/com/jwt/jwtstudy_youtube/jwt/LoginFilter.java
+++ src/main/java/com/jwt/jwtstudy_youtube/jwt/LoginFilter.java
@@ -1,5 +1,6 @@
 package com.jwt.jwtstudy_youtube.jwt;
 
+import com.jwt.jwtstudy_youtube.dto.CustomUserDetails;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,14 +8,22 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
-    public LoginFilter(AuthenticationManager authenticationManager) {
+    //생성자
+    public LoginFilter(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
     }
+
 
     @Override //필수
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -32,13 +41,24 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     //로그인 성공시 실행 메소드
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-        //(여기서 JWT를 발급하면 됨)
-        System.out.print("성공==============================");
+        //(여기서 JWT를 발급하면 됨 ->  JwtUtil)
+        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+        String username  = customUserDetails.getUsername();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+        GrantedAuthority auth = iterator.next();
+
+        String role = auth.getAuthority();
+
+        String token = jwtUtil.createJwt(username,role,120000L);
+
+        response.addHeader("Authorization", "Bearer " + token);
     }
+
 
     //로그인 실패시 실행 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-        System.out.println("실패==============================");
+        response.setStatus(404);
     }
 }
